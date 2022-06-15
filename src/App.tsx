3d5cf9411b02c32debf879ec81ad201715a113ee
eriks junior <eriksjunior13@gmail.com>
2022-06-15 13:44:51 -0300

    refactor: ajusta estrutura de pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import {Routes} from './routes'
+import {AppRoutes} from './routes'
 import '../App.css'
 
 function App() {
 
   return (
     <div className="App">
-      <Routes />
+      <AppRoutes />
     </div>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes as Switch, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { CreatePromotions, Home, Login } from "../pages";
 
-export const Routes = () =>{
+export const AppRoutes = () =>{
   return(
     <BrowserRouter>
-      <Switch>
+      <Routes>
         <Route  path="/" element={<Login />}/>
         <Route path="/home" element={<Home />}/>
         <Route path="/promocoes" element={<CreatePromotions />}/>
 
 
         <Route path="*" element={<Navigate to={"/home"}/>}/>
-      </Switch>
+      </Routes>
     </BrowserRouter>
   )
 }
