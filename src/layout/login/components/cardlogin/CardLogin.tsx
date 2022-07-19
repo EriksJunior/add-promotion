4740eaea68b9dcd993578af73f36f7cebb6483b3
eriks junior <eriksjunior13@gmail.com>
2022-07-18 21:28:20 -0300

    refactor

--- src/layout/login/components/cardlogin/CardLogin.tsx
+++ src/layout/login/components/cardlogin/CardLogin.tsx
@@ -8,7 +8,7 @@ import './CardLogin.scss'
 
 export const CardLogin: React.FC<ICardLogin> = ({ children }) => {
   return (
-    <div className="w-full flex flex-col justify-center items-center rounded-2xl bg-zinc-900 h-full bg-cyan-600 shadow-lg shadow-cyan-500/50">
+    <div className="w-full flex flex-col justify-center items-center rounded-2xl  h-full bg-cyan-600 shadow-lg shadow-cyan-500/50">
       {children}
     </div>
   )
