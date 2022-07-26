7b917e20232b463b749d8cdfd55c229ab4171c75
eriks junior <eriksjunior13@gmail.com>
2022-07-25 23:32:42 -0300

    refactor: removido obrigatoriedades do input adicionado dados a interface

--- src/shared/components/Input/Input.tsx
+++ src/shared/components/Input/Input.tsx
@@ -6,11 +6,14 @@ interface IInput {
   type?: string
   placeholder?: string
   size?: string
-  className: string
+  className?: string
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
+  name?: string
+  hidden?: boolean
 }
 
-export const Input: React.FC<IInput> = ({type, placeholder, size, className }) => {
+export const Input: React.FC<IInput> = ({ type, placeholder, size, className, onChange, name, hidden }) => {
   return (
-      <input className={className} type={type} placeholder={placeholder} style={{width: size, height: '25px', marginRight: '15px',  marginBottom: '15px', marginTop: '0px', maxWidth: '100%', minWidth: '20%', borderRadius: '0.125rem'}}/>
+    <input className={className} hidden={hidden} type={type} name={name} placeholder={placeholder} onChange={onChange} style={{ width: size, height: '25px', marginRight: '15px', marginBottom: '15px', marginTop: '0px', maxWidth: '100%', minWidth: '20%', borderRadius: '0.125rem' }} />
   )
 }
