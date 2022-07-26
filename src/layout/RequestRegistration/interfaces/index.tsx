8f1141fe1350209e2472d26a2402d4d651f70baa
eriks junior <eriksjunior13@gmail.com>
2022-07-25 22:37:08 -0300

    feat: criado interface para a company

---
+++ src/layout/RequestRegistration/interfaces/index.tsx
@@ -0,0 +1,14 @@
+export default interface ICompanyRegistration {
+  id: string
+  companyName: string
+  address: string
+  district: string
+  number: number
+  city: string
+  cep: string
+  cpfCnpj: string
+  email: string
+  telephone: string
+  password: string
+  secret: string
+}
--- src/layout/login/interfaces/index.tsx
+++ src/layout/login/interfaces/index.tsx
@@ -1,4 +1,4 @@
-export default interface ILogin{
-  login: string;
-  senha: string;
+export default interface ILogin {
+  login: string
+  senha: string
 }
